d97c40b0fd5298812deb8cb979e1e94544208178
Ashik Vetrivelu <vcashik@gmail.com>
2019-05-29 01:23:14 -0700

    Adding checkStyle

--- app/src/androidTest/java/com/cepheuen/elegantnumberbuttonsample/ApplicationTest.java
+++ app/src/androidTest/java/com/cepheuen/elegantnumberbuttonsample/ApplicationTest.java
@@ -1,19 +1,23 @@
 package com.cepheuen.elegantnumberbuttonsample;
 
-import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withResourceName;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static org.hamcrest.Matchers.both;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -28,7 +32,18 @@ public class ApplicationTest {
 
   @Test
   public void checkIfIncrementButtonChangesNumber() {
-    onView(both(withText("+")).and(withParent(withId(R.id.number_button)))).perform(click());
+    onView(both(withResourceName("add_btn"))
+        .and(withParent(withParent(withResourceName("number_button")))))
+        .perform(click());
+    onView(both(withResourceName("number_counter"))
+        .and(withParent(withParent(withResourceName("number_button")))))
+        .check(matches(withText("1")));
+    onView(both(withResourceName("add_btn"))
+        .and(withParent(withParent(withResourceName("number_button2")))))
+        .perform(click());
+    onView(both(withResourceName("number_counter"))
+        .and(withParent(withParent(withResourceName("number_button2")))))
+        .check(matches(withText("11")));
 
   }
 }
--- app/src/main/java/com/cepheuen/elegantnumberbuttonsample/MainActivity.java
+++ app/src/main/java/com/cepheuen/elegantnumberbuttonsample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cepheuen.elegantnumberbuttonsample;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -21,20 +22,20 @@ public class MainActivity extends AppCompatActivity {
 
     final TextView textView = findViewById(R.id.text_view);
 
-
+    final View contentView = findViewById(R.id.parent_view);
     elegantNumberButton.setRange(1, 5);
     elegantNumberButton.setOnClickListener(new ElegantNumberButton.OnClickListener() {
       @Override
       public void onClick(View view) {
         String number = elegantNumberButton.getNumber();
         textView.setText(number);
-        elegantNumberButton2.setNumber(number);
       }
     });
     elegantNumberButton.setOnValueChangeListener(new ElegantNumberButton.OnValueChangeListener() {
       @Override
       public void onValueChange(ElegantNumberButton view, int oldValue, int newValue) {
-        Log.d(TAG, String.format("oldValue: %d   newValue: %d", oldValue, newValue));
+        Snackbar.make(contentView, "Old Value: " + oldValue + " New Value: "
+            + newValue, Snackbar.LENGTH_SHORT).show();
       }
     });
 
@@ -43,42 +44,8 @@ public class MainActivity extends AppCompatActivity {
       public void onClick(View view) {
         String number = elegantNumberButton2.getNumber();
         textView.setText(number);
-        elegantNumberButton.setNumber(number);
       }
     });
-
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        try {
-          Thread.sleep(5000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-
-        runOnUiThread(new Runnable() {
-          @Override
-          public void run() {
-            // setting a number out of range notifying listeners
-            elegantNumberButton.setNumber("200", true);
-          }
-        });
-
-        try {
-          Thread.sleep(5000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-
-        runOnUiThread(new Runnable() {
-          @Override
-          public void run() {
-            // setting a number in range without notifying listeners
-            elegantNumberButton.setNumber("1");
-          }
-        });
-      }
-    }).start();
 
   }
 }
